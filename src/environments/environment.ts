40f40535c3708085ea14a9ea67fe4ef72120632f
Nina <nina.palumbo@grainger.com>
2018-08-20 10:09:11 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    // Initialize Firebase
+   firebase: {
+    apiKey: "AIzaSyA_Pie8se9qQMqAt199rkyE09CAVaawME8",
+    authDomain: "game-proj-cc727.firebaseapp.com",
+    databaseURL: "https://game-proj-cc727.firebaseio.com",
+    projectId: "game-proj-cc727",
+    storageBucket: "game-proj-cc727.appspot.com",
+    messagingSenderId: "498010247041"
+   }
+
 };
 
 /*
